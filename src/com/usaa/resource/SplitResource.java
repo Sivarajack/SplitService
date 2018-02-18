4a5557730c6b69da2f36db835c379087eef85e9d
sivaraj sivanesan <sivarajack@gmail.com>
2018-02-18 22:38:29 +0530

    stable code

--- src/com/usaa/resource/SplitResource.java
+++ src/com/usaa/resource/SplitResource.java
@@ -1,16 +1,26 @@
 package com.usaa.resource;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
 
+import com.usaa.entity.Groups;
+
 @Path("/message")
 public class SplitResource {
+	private static EntityManager em;
 
 	@GET
 	@Path("/{param}")
 	public Response printMessage(@PathParam("param") String msg) {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("Split");
+		em = emf.createEntityManager();
+		Groups group = new Groups(1,"Bank");
+		em.persist(group);
 
 		String result = "Restful example : " + msg;
 
